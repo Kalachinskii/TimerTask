e013ae497476b448ae39c1cd16e9bb73bec18765
kalachinskii_vl <kalachinskii_vl@mail.ru>
2025-02-14 17:31:27 +0300

    Style: добавлен стили backround в input при error

--- src/components/App/ui/App.jsx
+++ src/components/App/ui/App.jsx
@@ -4,12 +4,36 @@ import { Success } from '../../Success/success';
 import { ActiveTaskCard } from '../../ActiveTaskCard/ActiveTaskCard';
 import { TaskList } from '../../TaskList/taskList';
 import styles from '../app.module.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
 
 
 
 export function App() {
   const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const key = 'data';
+    // если нету ключа/данных - создаём хранилище
+    if (!localStorage.getItem(key)) {
+        localStorage.setItem(key, JSON.stringify([]));
+    }
+    // если данные есть - парсим
+    const data = JSON.parse(localStorage.getItem(key));
+    if (data) {
+      setItems(data.map(item => ({
+        ...item
+      })))
+    }
+
+}, []);
+
+  useEffect(() => {
+    if (items.length) {
+      // добовляем в хранилище данные
+      localStorage.setItem('data',JSON.stringify(items));
+    }
+  }, [items])
 
   const addItem = item => {
     setItems(oldItems => [...oldItems, {
@@ -18,6 +42,12 @@ export function App() {
       id: 1
     }]);
   };
+
+  // временно для рефакторинга
+  const clearLS = () => {
+    localStorage.clear(),
+    localStorage.setItem('data', JSON.stringify([]));
+  }
 
   return (
     <div className={styles['app']}>
@@ -30,6 +60,8 @@ export function App() {
           <Failed />
         </div>
       </div>
+      {/* очистка ЛС */}
+      <button style={{marginTop: '25px'}} onClick={clearLS}>Очистить ЛС</button>
     </div>
   )
 }
--- src/components/FormItem/ui/FormItem.jsx
+++ src/components/FormItem/ui/FormItem.jsx
@@ -1,20 +1,44 @@
+import { useState } from 'react';
 import styles from '../formItem.module.css';
 
 export function FormItem({addItem}) {
+    const [timeError, setTimeError] = useState('');
+    const [taskError, setTaskError] = useState('');
 
     const addTaskItem = (e) => {
         e.preventDefault();
         const formData = new FormData(e.target);
+        // обработка значения на пустоту
+        const taskValue = formData.get('task');
+        if (!taskValue.trim()) {
+            setTaskError('Введите задачу');
+            return;
+        }
+        setTaskError('');
+        // Обработка времени
+        const timeValue = formData.get('time');
+        if (!timeValue || isNaN(timeValue) || Number(timeValue) <= 0) {
+            setTimeError('Только положительное число.');
+            return;
+        }
+        // обнулить если были до этого ошибки
+        setTimeError('');
+        
         const formProps = Object.fromEntries(formData);
         addItem(formProps);
     }
 
+
     return (
         <>
+            <div>
+                {taskError && <p style={{ color: 'red' }}>{taskError}</p>}
+                {timeError && <p style={{ color: 'red' }}>{timeError}</p>}
+            </div>
             <form action="" className={styles['formItem']} onSubmit={addTaskItem}>
                 <div className={styles['formInput']}>
-                    <input type="text" name='task' placeholder='Введите задачу'/>
-                    <input type="text" name='time' placeholder='Введите время в секундах'/>
+                    <input type="text" name='task' placeholder='Введите задачу' className={taskError ? styles['error'] : ''} />
+                    <input type="text" name='time' placeholder='Введите время в секундах' className={timeError ? styles['error'] : ''}/>
                 </div>
                 <button className={styles['btn']}>
                     <i className="fa-solid fa-plus"></i>
